6bfd072f4496519db3f0000621f4df78865f79ad
daemon-cool <damian.damian.tofik@gmail.com>
2020-03-05 12:22:45 +0100

    Created drag and drop event listeners

---
+++ index.js
@@ -0,0 +1,21 @@
+let items = document.querySelectorAll('.container .list .item');
+
+items.forEach((item) => {
+    item.addEventListener('dragstart', (e, item) => {
+        console.log('dragstart');
+        e.dataTransfer.setData('application/my-app', e.target); //application/my-app
+    });
+});
+
+let list = document.querySelector('.container .list');
+list.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    console.log('dragover');
+});
+
+list.addEventListener('drop', (e) => {
+    console.log('drop');
+    let item = e.dataTransfer.getData('application/my-app');
+    console.log(item);
+    list.append(item);
+});
