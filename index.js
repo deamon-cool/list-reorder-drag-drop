b92d75e1afd8a089bc791bc0fd50a16247a86311
daemon-cool <damian.damian.tofik@gmail.com>
2020-03-18 15:05:23 +0100

    Changed to basis step

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let items = document.querySelectorAll('.container .list .item');
 
+console.log(items[1].id);
+
 items.forEach((item) => {
     item.addEventListener('dragstart', (e) => {
         console.log('dragstart');
-        e.dataTransfer.setData('application/my-app', e.target);
+        e.dataTransfer.setData('application/my-app', e.target.id);
     });
+
 
 });
 
@@ -12,12 +15,11 @@ let list = document.querySelector('.container .list');
 list.addEventListener('dragover', (e) => {
     e.preventDefault();
     console.log('dragover');
+    console.log(e);
 });
 
 list.addEventListener('drop', (e) => {
     console.log('drop');
     let item = e.dataTransfer.getData('application/my-app');
     console.log(item);
-
-    list.appendChild(item);
 });
