b1f6f2c6ebaa53a1a2bc08021746adb05e78e08c
daemon-cool <damian.damian.tofik@gmail.com>
2020-03-19 15:49:44 +0100

    Refactored code

--- index.js
+++ index.js
@@ -1,43 +1,57 @@
 let list = document.querySelector('.list');
 list.addEventListener('dragover', (e) => {
     e.preventDefault();
-    console.log('dragover');
+
     updateOrder(e.target, draggedItem);
 });
 
-
+// read items saved content from localStorage, create div and append to list
 for (let i = 0; i < localStorage.length; i++) {
     let temp = localStorage[i].split(':');
     let id = temp[0];
     let text = temp[1];
 
-    let div = document.createElement('div');
-    div.setAttribute('id', id);
-    div.setAttribute('class', 'item');
-    div.setAttribute('draggable', 'true');
-    div.textContent = text;
-
+    let div = createDivItem(id, 'item', true, text);
     list.appendChild(div);
 }
 
+// creats new item for list
+function createDivItem(id, divClass, draggable, text) {
+    let div = document.createElement('div');
+    div.setAttribute('id', id);
+    div.setAttribute('class', divClass);
+    div.setAttribute('draggable', draggable);
+    div.textContent = text;
+
+    return div;
+}
+
+
 let items = document.querySelectorAll('.list .item');
+addItemsListeners(items);
+
 let draggedItem;
 
-items.forEach((item) => {
-    item.addEventListener('dragstart', (e) => {
-        console.log('dragstart');
-        draggedItem = e.target;
-        draggedItem.style.opacity = '0.01';
+// adds listeners to items
+function addItemsListeners(items) {
+    items.forEach((item) => {
+        item.addEventListener('dragstart', (e) => {
+            draggedItem = e.target;
+
+            draggedItem.style.opacity = '0.01';
+        });
+
+        item.addEventListener('dragend', (e) => {
+            saveOrderToStorage(list);
+
+            draggedItem.style.opacity = '1';
+        });
+
     });
 
-    item.addEventListener('dragend', (e) => {
-        draggedItem.style.opacity = '1';
-        saveOrder(list);
-    });
+}
 
-});
-
-
+// update order of each item in the list when some is dragging
 function updateOrder(targetItem, draggedItem) {
     if (targetItem.className === draggedItem.className) {
         list.insertBefore(draggedItem, targetItem);
@@ -46,44 +60,24 @@ function updateOrder(targetItem, draggedItem) {
     }
 }
 
-function saveOrder(list) {
+// save order to localStorage of each item
+function saveOrderToStorage(list) {
     for (let i = 0; i < list.children.length; i++) {
         localStorage.setItem(i, list.children[i].id + ':' + list.children[i].textContent);
     }
 }
 
-
+// button and adds listeners to generete example list of items
 let buttonGenerate = document.querySelector('button');
 buttonGenerate.onclick = (e) => {
     e.preventDefault();
     if (list.children.length === 0) {
         for (let i = 0; i < 5; i++) {
-            let div = document.createElement('div');
-
-            div.setAttribute('id', 'item' + i);
-            div.setAttribute('class', 'item');
-            div.setAttribute('draggable', 'true');
-            div.textContent = 'Item ' + i;
-
+            let div = createDivItem('item' + i, 'item', true, 'Item ' + i);
             list.appendChild(div);
         }
 
         items = document.querySelectorAll('.list .item');
-
-        items.forEach((item) => {
-            item.addEventListener('dragstart', (e) => {
-                console.log('dragstart');
-                draggedItem = e.target;
-                draggedItem.style.opacity = '0.01';
-            });
-
-            item.addEventListener('dragend', (e) => {
-                draggedItem.style.opacity = '1';
-                saveOrder(list);
-            });
-
-        });
-
+        addItemsListeners(items);
     }
-
-}
+};
