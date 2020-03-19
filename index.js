133cfb06681fc0573cd73d8d9ca36dc926d3a759
daemon-cool <damian.damian.tofik@gmail.com>
2020-03-19 15:32:08 +0100

    Added functionality to creating new list when button was clicked

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ list.addEventListener('dragover', (e) => {
 });
 
 
-for(let i = 0; i < localStorage.length; i++) {
+for (let i = 0; i < localStorage.length; i++) {
     let temp = localStorage[i].split(':');
     let id = temp[0];
     let text = temp[1];
@@ -39,7 +39,7 @@ items.forEach((item) => {
 
 
 function updateOrder(targetItem, draggedItem) {
-    if(targetItem.className === draggedItem.className) {
+    if (targetItem.className === draggedItem.className) {
         list.insertBefore(draggedItem, targetItem);
     } else {
         list.appendChild(draggedItem);
@@ -47,7 +47,43 @@ function updateOrder(targetItem, draggedItem) {
 }
 
 function saveOrder(list) {
-    for(let i = 0; i < list.children.length; i++) {
+    for (let i = 0; i < list.children.length; i++) {
         localStorage.setItem(i, list.children[i].id + ':' + list.children[i].textContent);
     }
+}
+
+
+let buttonGenerate = document.querySelector('button');
+buttonGenerate.onclick = (e) => {
+    e.preventDefault();
+    if (list.children.length === 0) {
+        for (let i = 0; i < 5; i++) {
+            let div = document.createElement('div');
+
+            div.setAttribute('id', 'item' + i);
+            div.setAttribute('class', 'item');
+            div.setAttribute('draggable', 'true');
+            div.textContent = 'Item ' + i;
+
+            list.appendChild(div);
+        }
+
+        items = document.querySelectorAll('.list .item');
+
+        items.forEach((item) => {
+            item.addEventListener('dragstart', (e) => {
+                console.log('dragstart');
+                draggedItem = e.target;
+                draggedItem.style.opacity = '0.01';
+            });
+
+            item.addEventListener('dragend', (e) => {
+                draggedItem.style.opacity = '1';
+                saveOrder(list);
+            });
+
+        });
+
+    }
+
 }
